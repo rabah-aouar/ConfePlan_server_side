b7de67f692bd25a434adc96739413132db3efda7
rabah-aouar <88595484+rabah-aouar@users.noreply.github.com>
2022-05-05 04:52:09 +0100

    changes

--- users/views2/GetAllusersView.py
+++ users/views2/GetAllusersView.py
@@ -5,11 +5,15 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import LimitOffsetPagination
 from articles.serializers.ArticleDetailSerializer import ArticleDetailSerializer
 from users.serializers.UserProfileModificationSerializer import UserProfileModificationSerializer
+from rest_framework.filters import SearchFilter
 
 class GetAllusersView(ModelViewSet):
     ''' get the list of users
     '''
     serializer_class = UserProfileModificationSerializer
     pagination_class = LimitOffsetPagination
+    filter_backends = [SearchFilter]
+    search_fields = ['first_name','family_name','email']
+
     def get_queryset(self):
         return User.objects.filter(is_admin=False)
