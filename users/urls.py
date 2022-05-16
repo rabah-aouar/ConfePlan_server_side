8e4f8a6f52e75d073a6cd762d56e8d25d8c6c6eb
rabah-aouar <88595484+rabah-aouar@users.noreply.github.com>
2022-05-16 16:35:18 +0100

    add  get user with id

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@
 from ast import Delete
 
 from users.views2.ProfileView import ProfileView
-#from users.views2.LogoutView import LogoutView
+from users.views2.GetUserWithId import GetUserWithId
 from users.views2.RegisterView import RegisterView
 from users.views2.VerifyEmailView import VerifyEmailView
 from users.views2.AdminView import AdminView
@@ -14,7 +14,7 @@ from django.urls import path
 
 urlpatterns = [
     path('profile', ProfileView.as_view()),
-    #path('logout',LogoutView.as_view()),
+    path('<slug:id>',GetUserWithId.as_view()),
     path('register', RegisterView.as_view()),
     path('verify-account/<str:id>', VerifyEmailView.as_view()),
     path('admin/<str:id>',AdminView.as_view()),
