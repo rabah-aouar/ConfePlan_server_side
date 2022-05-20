7e433ae997f04669709807bed32fb3a644d7950d
rabah-aouar <88595484+rabah-aouar@users.noreply.github.com>
2022-05-20 20:38:21 +0100

    add another modifications

--- articles/serializers/ArticleDetailSerializer.py
+++ articles/serializers/ArticleDetailSerializer.py
@@ -34,6 +34,7 @@ class ArticleDetailSerializer(serializers.ModelSerializer):
     status=serializers.ReadOnlyField()
     #status=serializers.ReadOnlyField(required=False,default='pending',choices=article_status_choices)
     conference_id=serializers.PrimaryKeyRelatedField(queryset=Conference.objects.all(),many=False)
+    conference_id_name=serializers.ReadOnlyField()
     authors=AuthorSerializer(many=True,required=False,allow_null=True)
     user_id=serializers.StringRelatedField(read_only=True)
     reviewers=serializers.PrimaryKeyRelatedField(many=True,allow_null=True,read_only=True)
--- articles/views/WaitingForReviewArticle.py
+++ articles/views/WaitingForReviewArticle.py
@@ -5,6 +5,7 @@ from rest_framework.generics import GenericAPIView,ListAPIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import LimitOffsetPagination
+from articles.serializers.ArticleConferenceDetail import ArticleConferenceDetail
 from articles.serializers.ArticleDetailSerializer import ArticleDetailSerializer
 from users.models import User
 
@@ -12,7 +13,7 @@ class WaitingForReviewArticle(ModelViewSet):
     ''' get the list of articles for searcher
     '''
     #get list of articles for the searcher
-    serializer_class = ArticleDetailSerializer
+    serializer_class = ArticleConferenceDetail
     pagination_class = LimitOffsetPagination
     def get_queryset(self):
         return self.request.user.reviewrs_of_article.all()
