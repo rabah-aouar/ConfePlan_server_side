4c2503b502c8e7b7e9136c3fc317258aec192680
rabah-aouar <88595484+rabah-aouar@users.noreply.github.com>
2022-05-26 11:46:49 +0100

    add deadline to article

--- articles/views/ArticleView.py
+++ articles/views/ArticleView.py
@@ -1,3 +1,4 @@
+import datetime
 from rest_framework import status
 from articles.models.Article import Article, ArticleStatus
 from articles.serializers.ArticleDatesHistorySerializer import ArticleDatesHistorySerializer
@@ -7,6 +8,7 @@ from rest_framework.response import Response
 from rest_framework.parsers import FormParser,MultiPartParser,FileUploadParser
 
 from articles.serializers.ArticleStatusHistorySerializer import ArticleStatusHistorySerializer
+from conferences.models import Conference
 
 class ArticleView(GenericAPIView):
     """ end point to create an article object 
@@ -19,15 +21,18 @@ class ArticleView(GenericAPIView):
             waiting_id=waiting[0].id
             serializer= ArticleDetailSerializer(data=request.data)
             if serializer.is_valid(raise_exception=True):
-                serializer.validated_data['user_id']=request.user
-                serializer.save()
-                sr=ArticleDatesHistorySerializer(data={"Article":serializer.data['id']})
-                sr.is_valid(raise_exception=True)
-                sr.save()
-                sr5=ArticleStatusHistorySerializer(data={"type" :waiting_id,"Article":serializer.data['id']})
-                sr5.is_valid(raise_exception=True)
-                sr5.save()
-                return Response(data=serializer.data,status=status.HTTP_201_CREATED)
+                if serializer.validated_data["conference_id"].submition_deadline.replace(tzinfo=None) > datetime.datetime.now().replace(tzinfo=None):
+                    serializer.validated_data['user_id']=request.user
+                    serializer.save()
+                    sr=ArticleDatesHistorySerializer(data={"Article":serializer.data['id']})
+                    sr.is_valid(raise_exception=True)
+                    sr.save()
+                    sr5=ArticleStatusHistorySerializer(data={"type" :waiting_id,"Article":serializer.data['id']})
+                    sr5.is_valid(raise_exception=True)
+                    sr5.save()
+                    return Response(data=serializer.data,status=status.HTTP_201_CREATED)
+                else:
+                    return Response(data={'deadline has passed'},status=status.HTTP_400_BAD_REQUEST)
             else:
                 return Response(data=serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
