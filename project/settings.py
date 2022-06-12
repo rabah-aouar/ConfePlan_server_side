a93a98bd5d82cf3cc8c01f104e75dbd82ee9be17
rabah-aouar <88595484+rabah-aouar@users.noreply.github.com>
2022-06-12 17:18:25 +0100

    solve email problem

--- project/settings.py
+++ project/settings.py
@@ -171,7 +171,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'confplane@gmail.com'
-EMAIL_HOST_PASSWORD ='Confplan12'
+EMAIL_HOST_PASSWORD ='qkjyqzkkfzgnrxmy'
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'users.User'
